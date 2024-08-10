f2f92fc376b7c3ec54a8c180ac2e026bf78da378
unknown <vardhan70139@gnail.com>
2024-08-10 16:40:13 +0530

    add lazy loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,44 @@
-import React, { useState } from 'react'
-import Home from './pages/Home/Home'
-import Footer from './components/Footer/Footer'
-import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Cart from './pages/Cart/Cart'
-import LoginPopup from './components/LoginPopup/LoginPopup'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-import MyOrders from './pages/MyOrders/MyOrders'
+import React, { useState, lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Verify from './pages/Verify/Verify'
-import TrackOrder from './pages/TrackOrder/TrackOrder'
+import './App.css';
+
+const Home = lazy(() => import('./pages/Home/Home'));
+const Footer = lazy(() => import('./components/Footer/Footer'));
+const Navbar = lazy(() => import('./components/Navbar/Navbar'));
+const Cart = lazy(() => import('./pages/Cart/Cart'));
+const LoginPopup = lazy(() => import('./components/LoginPopup/LoginPopup'));
+const PlaceOrder = lazy(() => import('./pages/PlaceOrder/PlaceOrder'));
+const MyOrders = lazy(() => import('./pages/MyOrders/MyOrders'));
+const Verify = lazy(() => import('./pages/Verify/Verify'));
+const TrackOrder = lazy(() => import('./pages/TrackOrder/TrackOrder'));
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
 
-  const [showLogin,setShowLogin] = useState(false);
-  
   return (
     <>
-    <ToastContainer/>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin} />:<></>}
-      <div className='app'>
-        <Navbar setShowLogin={setShowLogin} name={name}/>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/cart' element={<Cart />}/>
-          <Route path='/order' element={<PlaceOrder />}/>
-          <Route path='/myorders' element={<MyOrders />}/>
-          <Route path='/verify' element={<Verify />}/>
-          <Route path='/TrackOrder' element={<TrackOrder/>}/>
-        </Routes>
-      </div>
-      <Footer />
+      <ToastContainer />
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
+      <Suspense fallback={<div className='verify'>
+      <div className="spinner"></div>
+    </div>}>
+        <div className='app'>
+          <Navbar setShowLogin={setShowLogin} />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/order' element={<PlaceOrder />} />
+            <Route path='/myorders' element={<MyOrders />} />
+            <Route path='/verify' element={<Verify />} />
+            <Route path='/TrackOrder' element={<TrackOrder />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Suspense>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
