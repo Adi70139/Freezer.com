7e1bf9fdb85e21c8514c8f732269ac0514220848
unknown <vardhan70139@gnail.com>
2024-09-21 11:38:15 +0530

    food display modify

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -5,25 +5,29 @@ import { StoreContext } from "../../Context/StoreContext";
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
+  
+  
+  const updatedList = food_list.filter((item) => category === "All" || category === item.category);
 
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-        {food_list.map((item) => {
-          if (category === "All" || category === item.category) {
-            return (
-              <FoodItem
-                key={item._id}
-                image={item.image}
-                name={item.name}
-                desc={item.description}
-                price={item.price}
-                id={item._id}
-              />
-            );
-          }
-        })}
+        {updatedList.length > 0 ? (
+          updatedList.map((item) => (
+            <FoodItem
+              key={item._id}
+              image={item.image}
+              name={item.name}
+              desc={item.description}
+              price={item.price}
+              id={item._id}
+            />
+          ))
+        ) : (
+          
+          <h2 style={{textAlign:"center", border:"2px solid black"}}>Food is awaited</h2>
+        )}
       </div>
     </div>
   );
