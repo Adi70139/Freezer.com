7bc7cb377e91d1a641802dbb943ca131dabf9dfd
Aditya Vardhan <inintr00329@DN9R44MYVK.local>
2025-07-09 09:39:09 +0530

    redirect file add

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,10 +3,31 @@ import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 import AppDownload from '../../components/AppDownload/AppDownload'
+import { useEffect } from 'react';
+import cookies from 'js-cookie';
 
 const Home = () => {
 
   const [category,setCategory] = useState("All")
+
+   useEffect(() => {
+      const token=cookies.get("oauthToken");
+      const email = cookies.get("oauthEmail");
+  
+      if (token && email) {
+        setToken(token);
+        setName(email);
+        localStorage.setItem("token", token);
+        loadCartData({ token });
+      } else {
+        navigate("/login");
+        toast.error("Something went wrong..Please try again")// In case token/email are missing
+      }
+  
+      cookies.remove("oauthToken");
+      cookies.remove("oauthEmail");
+  
+    }, []);
 
   return (
     <>
