c1b9f12e2f77d48564322152d04075012cecc9e3
Aditya Vardhan <inintr00329@DN9R44MYVK.local>
2025-07-10 21:21:28 +0530

    modified oauth redirect component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,34 @@
-import React, { useState,useContext } from 'react'
+import React, { useState,useContext,useEffect } from 'react'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 import AppDownload from '../../components/AppDownload/AppDownload'
-
+import { StoreContext } from "../../Context/StoreContext";
+import cookies from "js-cookie";
+import { toast } from "react-toastify";
 
 const Home = () => {
 
   const [category,setCategory] = useState("All")
+
+    const { setToken, setName, loadCartData } = useContext(StoreContext);
+  
+    useEffect(() => {
+      const token=cookies.get("oauthToken");
+      const email = cookies.get("oauthEmail");
+  
+      if (token && email) {
+        setToken(token);
+        setName(email);
+        localStorage.setItem("token", token);
+        loadCartData({ token });
+        toast.success("Logged in successfully");
+      }
+  
+      cookies.remove("oauthToken");
+      cookies.remove("oauthEmail");
+  
+    }, []);
   
   return (
     <>
